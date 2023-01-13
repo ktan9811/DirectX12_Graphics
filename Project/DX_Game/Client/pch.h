acc993b86fa800ff80df7c99d83ff2b4ef2cbf97
안기태 <ktan9811@gmail.com>
2023-01-13 15:05:43 +0900

    init DX12 game Project
    VS2022_DXProject

---
+++ Project/DX_Game/Client/pch.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma comment(lib, "Engine.lib")	// 전처리기를 활용한 추가 종속성
+
+#include "EnginePch.h"
+
+#include <vector>
+#include <memory>
+using namespace std;
---
+++ Project/DX_Game/Engine/EnginePch.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Windows.h>
+#include <tchar.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <array>
+#include <list>
+#include <map>
+
+#include "pch.h"
+#include "EnginePch.h"
+
+void HelloEngine()
+{
+
+}
